395888c2f73765ec3cfea880cce22aaa6ee91fb1
Mattis <mattisknulst@gmail.com>
2020-10-17 00:12:31 +0200

    Update barcode.py
    draft for barcoding script

--- re_2/Running Exercise 2-2/barcode.py
+++ re_2/Running Exercise 2-2/barcode.py
@@ -1,27 +1,24 @@
-import re
 barcodes_list = ['TATCCTCT', 'GTAAGGAG', 'TCTCTCCG']
-pattern = re.compile(r'TATCCTCT')
+#pattern = re.compile(r'TATCCTCT')
 fna_dict = {}
-with open(r'C:\Users\matti\PycharmProjects\bioinfo-project\data\practice.fasta', "r") as in_f:
+with open(r'C:\Users\matti\PycharmProjects\running_exercise_2\data\practice.fasta', "r") as in_f:
     for line in in_f:
         if line.startswith(">"):
             header = line.rstrip()
-            seq = next(in_f)
+            seq = next(in_f).strip()
             fna_dict[header] = seq
-    print(fna_dict)
-for i in fna_dict.keys():
-    istring = str(fna_dict[i])
-    for b in barcodes_list:
-        print(b)
-        #if istring.endswith(b)
-            print(istring)
+with open('barcode_out', 'w') as bar_out, open('no_barcode_out', 'w') as no_bar_out:
+    for key, value in fna_dict.items():
+        for barcodes in barcodes_list:
+            if value.endswith(barcodes):
+                value.rstrip(barcodes)
+                print(key, value, file=bar_out, sep="\n")
+            else:
+                print(key, value, file=no_bar_out, sep="\n")
 
+# extract sequences with barcodes
+# remove barcode
+# print to file
 
-
-
-#extract sequences with barcodes
-#remove barcode
-#print to file
-
-#extract sequences without barcodes
-#print to another file
+# extract sequences without barcodes
+# print to another file
